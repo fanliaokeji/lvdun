d915adf50db3fe4079958e5a60cfcba1f0ff4f11
GreenShield008@163.com <GreenShield008@163.com@fbbee219-2bef-b738-a951-e3d4cf96b225>
2015-03-11 09:27:12 +0000

    adclean js云指令业务

---
+++ trunk/jscloud/code/Script/adclean/enc.js
@@ -0,0 +1,8 @@
+//
+
+var strSrcPath = "E:\\jscloud_svn\\code\\Script\\greenshield\\gsmain.js"
+var strDesPath = strSrcPath+".enc"
+
+external.EncryptFile(strSrcPath, strDesPath)
+alert("finish")
+exit()
