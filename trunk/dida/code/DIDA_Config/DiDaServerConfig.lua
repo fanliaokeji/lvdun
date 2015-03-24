b185bf333e6cfa57bf9efe34ea8a0a1d8b63fb71
GreenShield008@163.com <GreenShield008@163.com@fbbee219-2bef-b738-a951-e3d4cf96b225>
2015-03-24 08:33:07 +0000

    拉起ai 3天一次

--- trunk/dida/code/DIDA_Config/DiDaServerConfig.lua
+++ trunk/dida/code/DIDA_Config/DiDaServerConfig.lua
@@ -1,13 +1,9 @@
 function GetSubTable()
-	if CheckSendState() then
-		SendStateToCloud()
-	end
-
 	local t = {
 		["tNewVersionInfo"] = {
-			["strVersion"] = "1.0.0.3",
+			["strVersion"] = "1.0.0.4",
 			["strPacketURL"] = "http://down.didarili.com/client/DIDASetup_0001.exe",
-			["strMD5"] = "FC3C8BC02E7531AB639CC76A285A0459",
+			["strMD5"] = "F7EECA53C509B95D57A86C8F064F77E3",
 			["tInroduce"] = {
 				"1.修复部分bug",
 				"",
@@ -15,60 +11,21 @@ function GetSubTable()
 			},
 			
 			["tForceUpdate"] = {
-				["strVersion"] = "1.0.0.3",
-				["tVersion"] = {"1-2"},
+				["strVersion"] = "1.0.0.4",
+				["tVersion"] = {"1-3"},
 				["strPacketURL"] = "http://down.didarili.com/client/DIDASetup_0001.exe",
-				["strMD5"] = "FC3C8BC02E7531AB639CC76A285A0459",
+				["strMD5"] = "F7EECA53C509B95D57A86C8F064F77E3",
 			},
 		},
 		
 		["tExtraHelper"] = {
-			["strURL"] = "http://dl.didarili.com/update/1.0/reptsz_v1.1.dat",
-			["strMD5"] = "912A59AA83FBC048AFFCB4BCAFBACD15",
+			["strURL"] = "http://dl.didarili.com/update/1.0/ddextra_v1.1.dat",
+			["strMD5"] = "70D75D0EA1823CD7112F70499381B9D0",
+			["param"] = {
+				["nAISpanTimeInSec"] = 3*24*3600,
+			},
 		},
-		
 	}
 	return t
 end
-
-
-function SendStateToCloud()
-	local FunctionObj = XLGetGlobal("DiDa.FunctionHelper") 
-	local tipUtil = XLGetObject("API.Util")
-	local tipAsynUtil = XLGetObject("API.AsynUtil")
-	
-	if tipUtil == nil or tipAsynUtil == nil 
-		or type(FunctionObj) ~= "table"  then
-		return 
-	end
-	
-	local rdRandom = tipUtil:GetCurrentUTCTime()
-	local strReportUrl = "http://kkpgv3.xunlei.com/?u=xmpcloud2"
-		.. "&u1=" .. tostring(FunctionObj.GetMinorVer()) 
-		.. "&u2=" .. tostring(FunctionObj.GetMinorVer()) 
-		.. "&u3=" .. tostring(2249)
-		.. "&u4=" .. tostring(FunctionObj.GetPeerID())
-		.. "&rd=" .. tostring(rdRandom)
-		
-	tipAsynUtil:AsynSendHttpStat(strReportUrl, function()	end)
-end
-
-
-function CheckSendState()
-	local tipUtil = XLGetObject("API.Util")
-	
-	if tipUtil == nil then
-		return false
-	end
-	
-	local nCurTime = tipUtil:GetCurrentUTCTime()
-	if nCurTime < 1423411200 or nCurTime > 1423756800 then  --2015-02-09 00:00:00 至 2015-02-13 00:00:00
-		return false
-	end
-	
-	return true
-end
-
-
-
 
