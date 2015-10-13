ef2ee623b8e9ea722cebf79093454be708df47b1
fanliaokeji <414335873@qq.com>
2015-10-13 17:15:53 +0800

    1.修改tslog的路径

--- dida/code/inc/tslog/tslog.h
+++ dida/code/inc/tslog/tslog.h
@@ -223,7 +223,7 @@ int _tmain(int argc, _TCHAR* argv[])
 #endif
 //Ä¬ÈÏÅäÖÃÄ¿Â¼
 #ifndef TSLOG_CONFIG_DIR
-#define TSLOG_CONFIG_DIR "C:\\DD_CONFIG\\"
+#define TSLOG_CONFIG_DIR "C:\\MYC_CONFIG\\"
 //#pragma message("warning: undefine macro 'TSLOG_CONFIG_DIR', default : [#define TSLOG_CONFIG_DIR \"" TSLOG_CONFIG_DIR "\"]")
 #endif
 
@@ -257,7 +257,7 @@ int _tmain(int argc, _TCHAR* argv[])
 #define MAX_HEX_DUMP_SIZE  		512
 #define MAX_ENVTSLOG_SIZE		128
 //no config, default
-#define DEFAULT_LOGFILE_PATH				"C:\\DDLOG\\"
+#define DEFAULT_LOGFILE_PATH				"C:\\MYCLOG\\"
 #define __TDEFAULT_LOGFILE_PATH				_T(DEFAULT_LOGFILE_PATH)
 #define DEFAULT_MAX_LOGFILE_SIZE			20480
 #define MAX_MAX_LOGFILE_SIZE				(DEFAULT_MAX_LOGFILE_SIZE * 100)
