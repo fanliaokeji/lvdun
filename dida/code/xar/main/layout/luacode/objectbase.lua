fe797f2d96d677021f7206b58f0893a0ed0d9f83
fanliaokeji <414335873@qq.com>
2015-12-12 11:03:53 +0800

    AddListener返回 callback 作为 cookie

--- dida/code/xar/main/layout/luacode/objectbase.lua
+++ dida/code/xar/main/layout/luacode/objectbase.lua
@@ -28,6 +28,8 @@ function ObjectBase:AddListener(event, callBack, ownerObj, bAddToEnd)
 	else
 		table.insert(self.listener[event], 1, {["callBack"] = callBack, ["ownerObj"] = ownerObj})
 	end
+	
+	return callBack
 end
 
 function ObjectBase:DispatchEvent(event, ...)
