0b7db09fa3082a5b68c7b08ca37275a9290a55e1
fanliaokeji <414335873@qq.com>
2016-05-16 20:38:29 +0800

    地址栏控件

---
+++ aitushow/code/xar/main/layout/basecontrol/AddressEdit.xml.lua
@@ -0,0 +1,21 @@
+local Helper = XLGetGlobal("Helper")
+
+function SetPath(self)
+
+end
+
+function GetPath(self)
+
+end
+
+function OnClickLeftArrow(self)
+
+end
+
+function OnClickRightArrow(self)
+
+end
+
+function OnClickUpArrow(self)
+
+end
