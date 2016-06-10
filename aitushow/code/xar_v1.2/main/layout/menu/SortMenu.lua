864de0ce7f6ab4aa32a5cf4e31d4a69e3a961699
fanliaokeji <414335873@qq.com>
2016-06-10 11:44:39 +0800

    使用图片池的排序接口

--- aitushow/code/xar_v1.2/main/layout/menu/SortMenu.lua
+++ aitushow/code/xar_v1.2/main/layout/menu/SortMenu.lua
@@ -4,80 +4,26 @@ if not GreenShieldMenu then
 	XLSetGlobal("GreenShieldMenu", GreenShieldMenu)
 end
 
-local function GetThumbContainerObj(self)
-	local tree = self:GetOwner()
-	local wnd  = tree:GetBindHostWnd() 
-	local userData = wnd:GetUserData()
-	
-	return userData and userData.thumbContainerObj
-end
 
-local function QuickSort(tPictures, sortKey)
-	table.sort(tPictures,function (v1,v2)
-									if sortKey == "FileName" then
-										local name1 = string.match(v1.szPath, ".+[\\/]([^?]+)")
-										local name2 = string.match(v2.szPath, ".+[\\/]([^?]+)")
-										if name1 < name2 then
-											return true
-										end
-										return false
-									end
-									if v1[sortKey] < v2[sortKey] then
-										return true
-									else
-										return false
-									end
-							end)
-end
-
+local bReverse = true
 local menuFunTable = {}
 function menuFunTable.OnSelect_FileName(self)
-	local thumbContainerObj = GetThumbContainerObj(self)
-	if not thumbContainerObj then
-		XLMessageBox("hj")
-		return
-	end
-	local attr = thumbContainerObj:GetAttribute()
-	local tPictures = attr.tPictures
-	
-	QuickSort(tPictures, "FileName")
-	attr.pageManager:Init(thumbContainerObj, tPictures)
+	-- bReverse = not bReverse
+	ImagePool:Sort("FileName")
 end
 
 function menuFunTable.OnSelect_Size(self)
-	local thumbContainerObj = GetThumbContainerObj(self)
-	if not thumbContainerObj then
-		return
-	end
-	local attr = thumbContainerObj:GetAttribute()
-	local tPictures = attr.tPictures
-	
-	QuickSort(tPictures, "uFileSize")
-	attr.pageManager:Init(thumbContainerObj, tPictures)
+	Helper:SaveLuaTable(ImagePool.tPictures, "E:\\before.lua")
+	ImagePool:Sort("FileSize")
+	Helper:SaveLuaTable(ImagePool.tPictures, "E:\\after.lua")
 end
 
 function menuFunTable.OnSelect_OperateTime(self)
-	local thumbContainerObj = GetThumbContainerObj(self)
-	if not thumbContainerObj then
-		return
-	end
-	local attr = thumbContainerObj:GetAttribute()
-	local tPictures = attr.tPictures
-	
-	QuickSort(tPictures, "utcLastWriteTime")
-	attr.pageManager:Init(thumbContainerObj, tPictures)
+	ImagePool:Sort("LastWriteTime")
 end
 
 function menuFunTable.OnSelect_Ext(self)
-	local thumbContainerObj = GetThumbContainerObj(self)
-	if not thumbContainerObj then
-		return
-	end
-	local attr = thumbContainerObj:GetAttribute()
-	local tPictures = attr.tPictures
-	
-	QuickSort(tPictures, "szExt")
-	attr.pageManager:Init(thumbContainerObj, tPictures)
+	ImagePool:Sort("ExtName")
 end
 
 
