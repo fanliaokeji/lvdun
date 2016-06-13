40c7ba0067162fbf825b8ac5eddab01f6e9aa86f
fanliaokeji <414335873@qq.com>
2016-06-13 15:08:44 +0800

    删除无效配置项

--- aitushow/code/xar_v1.2/main/layout/luacode/UserConfig.lua
+++ aitushow/code/xar_v1.2/main/layout/luacode/UserConfig.lua
@@ -4,9 +4,26 @@ local UserConfig = ObjectBase:New()
 XLSetGlobal("UserConfig", UserConfig)
 UserConfig.path = Helper:GetUserDataDir().."\\kuaikan\\UserConfig.lua"
 
+function delinvalid(tcfg)
+	local keylist = {}
+	for k, v in pairs(tcfg) do
+		if string.find(string.lower(tostring(k)), "_del$") then
+			keylist[#keylist+1] = k
+		end
+		if type(v) == "table" then
+			delinvalid(v)
+		end
+	end
+	for _, v in ipairs(keylist) do
+		tcfg[v] = nil
+	end
+end
+
 function UserConfig:Load()
 	--从配置文件里读取上次的配置
 	self.configData = Helper:LoadLuaTable(self.path) or {}
+	--删除失效的配置项
+	delinvalid(self.configData)
 end
 
 --带#的设置项只对当前进程有效
