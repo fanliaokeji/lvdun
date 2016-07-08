d1a8a91cae3b4356fd545a51a2475a7d4c8e40bf
fanliaokeji <414335873@qq.com>
2016-07-08 18:17:36 +0800

    版本升级到1.0.0.11

--- aitushow/code/aitushow_v1.2/autobuild.bat
+++ aitushow/code/aitushow_v1.2/autobuild.bat
@@ -16,6 +16,7 @@ if not %errorlevel%==0 (
 	echo rebuild %sln% failed
 	goto done
 )
+Call %~dp0..\xar_v1.2\pack\pack.bat
 cd /d "%basedir:~1,-1%Release"
 call :printversion "%cd%\kuaikantu.exe" kuaikantu.exe
 for /r %%i in (*.dll) do (
